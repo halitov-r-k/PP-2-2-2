e3795b8db85c23cb3f7deed5598a1e6a26518079
halitov-r-k <halitov.r.k@gmail.com>
2022-09-13 22:10:09 +0500

    2022-09-13T22:00

--- src/main/java/web/models/Car.java
+++ src/main/java/web/models/Car.java
@@ -2,30 +2,24 @@ package web.models;
 
 public class Car {
     Long id;
+    String name;
+    int speed;
+    public Car(Long id, String name, int speed) {
+        this.id = id;
+        this.name = name;
+        this.speed = speed;
+    }
 
     public Long getId() {
         return id;
     }
 
-
-    String name;
-
     public String getName() {
         return name;
     }
 
-
-    int speed;
-
     public int getSpeed() {
         return speed;
-    }
-
-
-    public Car(Long id, String name, int speed) {
-        this.id = id;
-        this.name = name;
-        this.speed = speed;
     }
 
     @Override
